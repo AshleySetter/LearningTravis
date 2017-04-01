545c501ac8375f529f5938a409032f997bb6c35d
AshleySetter <ajs3g11@soton.ac.uk>
2017-04-01 17:30:00 +0100

    added function to bbe tested and pytest test script

---
+++ UnecessaryMath.py
@@ -0,0 +1,7 @@
+def multiply(a, b):
+    """
+    multiplies 2 python objects,
+    a and b and returns the result
+    """
+    result = a*b
+    return result
---
+++ test_UnecessaryMath.py
@@ -0,0 +1,10 @@
+import UnecessaryMath
+
+def test_numbers_3_4():
+    assert UnecessaryMath.multiply(3,4) == 12
+
+def test_strings_a_3():
+    assert UnecessaryMath.multiply('a',3) == 'aaa'
+
+def test_strings_b_4():
+    assert UnecessaryMath.multiply('b',4) == 'bbbb'
